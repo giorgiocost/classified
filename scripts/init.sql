6794629d3698e04cf585f384ae5a5a2fab53320b
root <root@giorgio.costa>
2018-07-17 01:44:48 -0300

    init app

---
+++ scripts/init.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE IF NOT EXISTS mydatabase;  
+ USE mydatabase;  
+   
+ CREATE TABLE IF NOT EXISTS mytable (  
+  id INT(11) PRIMARY KEY AUTO_INCREMENT,  
+  descricao VARCHAR(50)  
+ );  
+   
