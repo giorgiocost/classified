c4973b3f7d89a70b83f865c127d281dd869e4615
root <root@giorgio.costa>
2018-07-19 23:57:10 -0300

    ajust

--- app/pages/header.php
+++ app/pages/header.php
@@ -18,12 +18,21 @@
                 <a href="http://" class="navbar-brand">Classificados</a>
             </div>
             <ul class="nav navbar-nav navbar-right">
-                <li><a href="http://">Cadastre-se</a></li>
-                <li><a href="http://">Login</a></li>
-                <!--
-                <li><a href="http://">Meus Anúncios</a></li>
-                <li><a href="http://">Sair</a></li>
-                -->
+
+        <?php        
+            if(isset($_SESSION['cLogin']) && !empty($_SESSION['cLogin'])){ 
+        ?>
+                <li><a href="meus-anuncios.php">Meus Anúncios</a></li>
+                <li><a href="sair.php">Sair</a></li>
+        <?php 
+            }else{ 
+        ?>
+                <li><a href="cadastre-se.php">Cadastre-se</a></li>
+                <li><a href="login.php">Login</a></li>
+        <?php 
+            } 
+        ?>
+   
             </ul>
         </div>
     </nav>
