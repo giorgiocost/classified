07e78d9bb3f12a06f55f12720806c0a98c32fdfe
giorgiocost <gegiorgio1991@gmail.com>
2018-07-26 15:17:35 -0300

    init api

--- htdocs/classes/anuncios.class.php
+++ htdocs/classes/anuncios.class.php
@@ -1,7 +1,18 @@
 <?php
 
+session_start();
+
 class Anuncios{
     public function getMeusAnuncios(){
+        global $pdo;
+        $sql = $pdo->prepare("SELECT * FROM anuncios WHERE id_usuarios = :id_usuarios ");
+        $sql->bindValue(":id_usuarios", $_SESSION['cLogin']);
+        $sql->execute();
+
+        if($sql->rowCount() > 0){
+            $array = $sql->fetchAll();
+        }
+        return $array;
 
     }
 }
